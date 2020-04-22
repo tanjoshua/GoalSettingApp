67d9eb7e7eb6b97f3a3311343d3c73d734232d7e
Joshua Tan <jtanjoshua@gmail.com>
2020-04-22 21:51:11 +0800

    store user input

--- App.js
+++ App.js
@@ -1,12 +1,29 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { StyleSheet, Text, View, TextInput, Button } from 'react-native';
 
 export default function App() {
+  const [enteredGoal, setEnteredGoal] = useState('');
+
+  const enterGoalHandler = (enteredText) => {
+    setEnteredGoal(enteredText);
+  };
+
+  const addGoalHandler = () => {
+    console.log(enteredGoal);
+  };
+
   return (
     <View style={styles.screen}>
       <View style={styles.inputContainer}>
-        <TextInput placeholder="Course goal" style={styles.textInput} />
-        <Button title="Add" />
+        <TextInput
+          placeholder="Course goal"
+          style={styles.textInput}
+          onChangeText={enterGoalHandler}
+          value={enteredGoal}
+        />
+        <Button title="Add"
+          onPress={addGoalHandler}
+        />
       </View>
       <View>
 
