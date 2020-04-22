bf2a031dfa8599bbb80075074217c781ff6a2f91
Joshua Tan <jtanjoshua@gmail.com>
2020-04-22 20:06:19 +0800

    Basic skeleton

---
+++ App.js
@@ -0,0 +1,34 @@
+import React from 'react';
+import { StyleSheet, Text, View, TextInput, Button } from 'react-native';
+
+export default function App() {
+  return (
+    <View style={styles.screen}>
+      <View style={styles.inputContainer}>
+        <TextInput placeholder="Course goal" style={styles.textInput} />
+        <Button title="Add" />
+      </View>
+      <View>
+
+      </View>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  screen: {
+    padding: 50
+  },
+
+  inputContainer: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center'
+  },
+
+  textInput: {
+    width: '80%',
+    borderBottomColor: 'black',
+    borderBottomWidth: 1
+  }
+});
